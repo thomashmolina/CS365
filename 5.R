cbcc4c5e1ce703c926886cdc7d546e7cac7d353a
thomashmolina <thomas.h.molina@gmail.com>
2019-09-09 10:35:20 -0600

    5th done

---
+++ 5.R
@@ -0,0 +1,4 @@
+numLions <- 42
+numTigers <- 17
+country <- "South Africa"
+print(paste0("The number of lions in South Africa is ",numLions, ".", " The number of tigers in South Africa is ", numTigers, "."))
